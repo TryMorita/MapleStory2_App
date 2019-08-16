7bb6fd164cfa8f3efed400b1bfe0554a9d8a4c6e
Declan Morita <decmor95gw@gmail.com>
2019-08-16 13:56:37 -0700

    Tutorial Xam App
    Followed a basic Xam app tutorial

---
+++ OneMinParseMS2/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace OneMinParseMS2
+{
+    [Activity(Label = "OneMinParseMS2", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView TxtNumber;
+
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.content_main);
+
+            TxtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            TxtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            TxtNumber.Text = (--number).ToString();
+
+        }
+    }
+}
